6c60f182a0c22cc704caa0e22f2ec8c32bdb1712
ridhoajibx <41810963+ridhoajibx@users.noreply.github.com>
2020-08-21 03:03:54 +0700

    create paper components and install library prop-types

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
 import React from "react";
 import "./styles.css";
 
+import Paper from "./components/Paper";
+
 export default function App() {
   return (
-    <div className="container">
-      <div className="frame">
-        <section className="header">
-          <button className="header-btn main-black-color active">Add</button>
-          <h1 className="header-title">todo Lists</h1>
-          <button className="header-btn main-red-color active">Clear</button>
-        </section>
+    <Paper>
+      <section className="header">
+        <button className="header-btn main-black-color active">Add</button>
+        <h1 className="header-title">todo Lists</h1>
+        <button className="header-btn main-red-color active">Clear</button>
+      </section>
 
-        <section className="add">
-          <form className="add-form">
-            <input className="add-input" type="text" name="" id="" />
-            <button className="add-btn main-black-color active">Add</button>
-          </form>
-        </section>
+      <section className="add">
+        <form className="add-form">
+          <input className="add-input" type="text" name="" id="" />
+          <button className="add-btn main-black-color active">Add</button>
+        </form>
+      </section>
 
-        <section className="todos">
-          <div className="todo">
-            <span className="todo-text">Learning React!</span>
-          </div>
+      <section className="todos">
+        <div className="todo">
+          <span className="todo-text">Learning React!</span>
+        </div>
 
-          <div className="todo">
-            <span className="todo-text">Learning React Hooks!</span>
-          </div>
+        <div className="todo">
+          <span className="todo-text">Learning React Hooks!</span>
+        </div>
 
-          <div className="todo">
-            <span className="todo-text">Learning Styling in React!</span>
-          </div>
-        </section>
-      </div>
-    </div>
+        <div className="todo">
+          <span className="todo-text">Learning Styling in React!</span>
+        </div>
+      </section>
+    </Paper>
   );
 }
---
+++ src/components/Paper.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+// props adalah sebuah object yg berisi sbuah children
+
+const Paper = ({ children }) => {
+  return (
+    <div className="container">
+      <div className="frame">{children}</div>
+    </div>
+  );
+};
+
+Paper.PropTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ])
+};
+
+export default Paper;
