1f20b686af5dd42d2bcea75d833219e1313b8b2c
ridhoajibx <41810963+ridhoajibx@users.noreply.github.com>
2020-08-21 02:31:44 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
           <section className="header">
             <button className="header-btn main-black-color active">Add</button>
             <h1 className="header-title">todo Lists</h1>
-            <button className="header-btn main-red-color">Clear</button>
+            <button className="header-btn main-red-color active">Clear</button>
           </section>
 
           <section className="add">
@@ -21,19 +21,15 @@ export default function App() {
 
           <section className="todos">
             <div className="todo">
-              <span className="todo-text">Learning React</span>
+              <span className="todo-text">Learning React!</span>
             </div>
 
             <div className="todo">
-              <span className="todo-text">Learning React</span>
+              <span className="todo-text">Learning React Hooks!</span>
             </div>
 
             <div className="todo">
-              <span className="todo-text">Learning React</span>
-            </div>
-
-            <div className="todo">
-              <span className="todo-text">Learning React</span>
+              <span className="todo-text">Learning Styling in React!</span>
             </div>
           </section>
         </div>
