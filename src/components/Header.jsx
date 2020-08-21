3f85288277951d02f184a6c8bde674eb14d1dc1e
ridhoajibx <41810963+ridhoajibx@users.noreply.github.com>
2020-08-22 03:46:24 +0700

    create conditional renderring for showAdd btn

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Header = ({ showAddToogle }) => {
+const Header = ({ showAddToogle, showAdd }) => {
   return (
     <section className="header">
       <button
         className="header-btn main-black-color active"
         onClick={showAddToogle}
       >
-        Add
+        {/* This is ternarry operator for showAdd state props */}
+        {showAdd ? "Finish" : "Add"}
       </button>
       <h1 className="header-title">todo Lists</h1>
       <button className="header-btn main-red-color active">Clear</button>
@@ -17,7 +18,8 @@ const Header = ({ showAddToogle }) => {
 };
 
 Header.propTypes = {
-  showAddToogle: PropTypes.func.isRequired
+  showAddToogle: PropTypes.func.isRequired,
+  showAdd: PropTypes.bool.isRequired
 };
 
 export default Header;
