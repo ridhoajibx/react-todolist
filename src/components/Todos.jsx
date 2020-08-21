1528bfdffbf025cbb559afc1b2ad4a86ff5fc949
ridhoajibx <41810963+ridhoajibx@users.noreply.github.com>
2020-08-22 06:29:21 +0700

    create placeholder with conditional method

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -6,17 +6,25 @@ import Todo from "./Todo";
 const Todos = ({ todos, completeTodo }) => {
   return (
     <section className="todos">
-      {todos.map((todo, index) => {
-        return (
-          <Todo
-            key={index}
-            text={todo.text}
-            isCompleted={todo.isCompleted}
-            completeTodo={completeTodo}
-            index={index}
-          />
-        );
-      })}
+      {todos.length > 0 &&
+        todos.map((todo, index) => {
+          return (
+            <Todo
+              key={index}
+              text={todo.text}
+              isCompleted={todo.isCompleted}
+              completeTodo={completeTodo}
+              index={index}
+            />
+          );
+        })}
+      {todos.length === 0 && (
+        <div className="todo-placeholder-text">
+          You don't have todos on the list! Add todo by clicking{" "}
+          <span className="add-btn-placeholder-text main-black-color">Add</span>{" "}
+          button on the top left corner
+        </div>
+      )}
     </section>
   );
 };
