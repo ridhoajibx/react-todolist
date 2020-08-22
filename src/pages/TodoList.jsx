343417c1884fcb7e8e6f424b3605538073421354
ridhoajibx <41810963+ridhoajibx@users.noreply.github.com>
2020-08-23 06:29:24 +0700

    add conditional if showAdd button = true (clear btn = unfunction)

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -24,7 +24,7 @@ const TodoList = () => {
     }
   };
 
-  const clearTodos = () => setTodos([]);
+  const clearTodos = () => !showAdd && setTodos([]);
 
   const completeTodo = (index) => {
     const addedTodo = [...todos];
